170c5cb76406dd53550dddb2f601eb9e2612dd5d
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2022-05-29 20:18:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,6 @@ while True:
     print(f"\nKiritgan {nomer} raqamingiz xato !/n")
 
 # 5.Veb sahifani ajratib ollish
-domen = []
 matnlar = """Assalom alaykum hurmatli do'stlar. Navbatdagi darsimiz YouTubega yuklandi: https://youtu.be/vsxJPRLXpgI
 Ushbu darsimizda unittest moduli yordamida klasslarning xususiyatlar va metodlarini tekshiruvchi dastur yozishni o'rganamiz. Bugungi dars manzili: https://python.sariq.dev/testing/37-klass-test"""
 
